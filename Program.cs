5321b9e51d0e1640e89aadaa76b3647776badd3c
Александр Гаврилов <aleksandrgavrilov7229@gmail.com>
2024-12-21 21:38:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using ClassLibrary08;
+
+class Program
+{
+    public static void Main()
+    {
+        string serializedLion =
+            "{\"Country\":\"Kenya\",\"HideFromOtherAnimals\":true,\"Name\":\"Lion\",\"WhatAnimal\":\"Lion\",\"Classification\":1}\n";
+        Lion? lion = JsonSerializer.Deserialize<Lion>(serializedLion);
+        Console.WriteLine($"Lion name: {lion?.Name}");
+        Console.WriteLine($"Lion country: {lion?.Country}");
+    }
+}
